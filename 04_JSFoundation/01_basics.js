44423529998b709cd943b029bc05a9e91aa0937d
AyushRawat1718 <ayushrawat1718@gmail.com>
2025-08-13 21:48:23 +0530

    js basics

---
+++ 04_JSFoundation/01_basics.js
@@ -0,0 +1,40 @@
+// Basic printing functon
+console.log("Hello World"); //Preferred
+
+console.table({ firstName: "Ayush", lastName: "Rawat" }); //In table format
+
+let gameName = "spiderman";
+gameName = "batman";
+
+console.log(gameName);
+
+const new_gameName = "spiderman";
+new_gameName = "batman";
+
+console.log(new_gameName);
+
+// Data Types
+
+// String – Represents textual data.
+let name = "Ayush";
+
+// Number – Represents integers and floating-point numbers.
+let age = 25;
+
+// Boolean – Represents true or false values.
+let isLoggedIn = true;
+
+// BigInt – Represents very large integers beyond Number limit.
+let bigNumber = 12345678901234567890n; //The n at the end tells that this number is a bigint.
+
+// Undefined – Variable declared but no value assigned.
+let x;
+
+// Null – Represents intentional absence of any value.
+let y = null;
+
+// Symbol – Represents a unique and immutable identifier.
+let id = Symbol("unique");
+
+// Object – A collection of key-value pairs.
+let person = { name: "Ayush", age: 25 };
